3dc4190485e560c9e23d70cf45f8cf0d775a01df
parth sharma <77784457+parthsharma365628@users.noreply.github.com>
2021-05-19 17:11:38 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+
+    fill("white")
+    textSize(15)
+    text("A B T S A D E A",300,150)
+    fill("lightblue")
+    text("Hint: It is useful for storing your in-game secrets ",300,170)
+
+
+    fill("white")
+    textSize(15)
+    text("F C O T N I U N",700,250)
+    fill("lightblue")
+    text("Hint: It will perform your task, like it or not",700,270)
+    
+
+
+
+}
